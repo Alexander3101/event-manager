3fe2cf91c4c05eb37f081843e30c96df77ddaa2b
Unknown <chip@ocbs.pro>
2017-06-22 13:16:56 +0300

    users added, must be ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # если вошёл или вышел, остаться на этой же странице
+  def after_sign_in_path_for(resource_or_scope)
+    request.referrer
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
 end
