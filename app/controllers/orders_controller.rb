c62626138af87c1a9b39076bf333525e94d0091c
Dale <dale@ocbs.pro>
2017-06-26 13:54:48 +0300

    controllers fixed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,22 @@
 class OrdersController < ApplicationController
+  def new
+    @order = Order.new
+    @events = Event.all
+    @room_id = params[:room_id]
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.event.rooms << @order.room
+    
+    if @order.save
+      redirect_to room_path(@order.room_id)
+    else
+      redirect_to '/rooms'
+    end
+  end
+
+  def order_params
+    params.require(:order).permit(:room_id, :begin_datetime, :end_datetime, :event_id)
+  end
 end
