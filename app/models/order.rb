3301c301e44cf58bb6ec83491d442c72c6497a86
ChipOCBS <chip@ocbs.pro>
2017-06-28 23:38:31 +0300

    hotfix

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,5 +5,5 @@ class Order < ApplicationRecord
   # проверка на присутствие
   validates :begin_datetime, :end_datetime, :room_id, :event_id, presence: true
   # проверка на integer и not_null
-  validates :user_id, :room_id, numericality: { only_integer: true }
+  validates :event_id, :room_id, numericality: { only_integer: true }
 end
