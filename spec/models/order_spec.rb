03052c502b4d5af6fe6d73afd3e589a9bcbdf938
Darya Tikhomirova <tihodrik@gmail.com>
2017-06-28 14:47:25 +0300

    not finished yet

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  let (:r_id) { Room.order(id: :desc).first[:id] }
-  let (:e_id) { Event.order(id: :desc).first[:id] }
+  let(:u_id) do
+    User.order(id: :desc).first[:id]
+  end
+
+  let(:event) do
+    Event.order(id: :desc).first[:id]
+  end
+  let(:invalid_event_id) { event[:id] + 1 }
+
+  let(:room) do
+    Room.order(id: :desc).first[:id]
+  end
+  let(:invalid_room_id) { room[:id] + 1 }
+
   let(:order) do
     Order.new(
-      begin_datetime: DateTime.now,
-      end_datetime: DateTime.now + 15.minutes,
-      room_id: r_id,
-      event_id: e_id
+      begin_datetime: event.begin_datetime,
+      end_datetime: event.end_datetime,
+      room_id: room.id,
+      event_id: event.id
     )
   end
 
@@ -39,6 +51,33 @@ RSpec.describe Order, type: :model do
       end
     end
 
+    context "if order length is less than 15 minutes" do
+      it "is not valid" do
+        order[:end_datetime] = order[:begin_datetime] + 2.minutes
+        expect(order).not_to be_valid
+      end
+    end
+
+    context "if order length is less than event length" do
+      it "is not valid" do
+        order[:begin_datetime] = event[:begin_datetime]
+        order[:end_datetime] = event[:end_datetime] - 1.minute
+        expect(order).not_to be_valid
+      end
+    end
+
+    # JUST STARTED
+    context "if order time is beyond of the room work time" do
+      it "is not valid" do
+        b_date = DateTime.now
+        b_date.hours = room.begin_work_time.hours
+
+        order[:begin_datetime] = event[:begin_datetime]
+        order[:end_datetime] = event[:end_datetime] - 1.minute
+        expect(order).not_to be_valid
+      end
+    end
+
     context "if nil room_id is set" do
       it "is not valid" do
         order[:room_id] = nil
@@ -48,7 +87,7 @@ RSpec.describe Order, type: :model do
 
     context "if missed room_id is set" do
       it "is not valid" do
-        order[:room_id] = r_id + 1
+        order[:room_id] = invalid_room_id
         expect(order).not_to be_valid
       end
     end
@@ -62,7 +101,7 @@ RSpec.describe Order, type: :model do
 
     context "if missed event_id is set" do
       it "is not valid" do
-        order[:event_id] = e_id + 1
+        order[:event_id] = invalid_event_id
         expect(order).not_to be_valid
       end
     end
