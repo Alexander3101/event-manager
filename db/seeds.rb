ee5be5fcf4b6f9ee6e3dcf1d99016f86399d00b6
DaleOCBS <dale@ocbs.pro>
2017-06-29 12:48:32 +0300

    added events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,55 +1,73 @@
 users_number = 8
-events_number = 10
+events_number = 12
 rooms_number = 6
 
 # Пользователи
 users_number.times do
   email_tmp = Faker::Internet.unique.email
   User.create(
-  email: email_tmp,
-  password: email_tmp[0,2] + "12345"
+    email: email_tmp,
+    password: email_tmp[0, 2] + '12345'
   )
 end
 
 User.create(
-email: "admin@example.com",
-password: "password",
-role: "admin"
+  email: 'admin@example.com',
+  password: 'password',
+  role: 'admin'
 )
 
 User.create(
-email: "admin@admins.net",
-password: "admin2",
-role: "admin"
+  email: 'admin@admins.net',
+  password: 'admin2',
+  role: 'admin'
 )
 
 # События
 events_number.times do |i|
   title_tmp = Faker::Book.title
   Event.create(
-  title: title_tmp,
-  description: title_tmp + title_tmp + title_tmp,
-  begin_datetime: DateTime.now + i.minutes,
-  end_datetime: DateTime.now + 10.days,
-  user_id: rand(0..users_number-1)
+    title: title_tmp,
+    description: title_tmp + title_tmp + title_tmp,
+    begin_datetime: DateTime.now + i.minutes,
+    end_datetime: DateTime.now + 10.days,
+    user_id: rand(0..users_number - 1)
   )
 end
-
+Event.create(
+  title: 'My_event',
+  description: 'la - la - la',
+  begin_datetime: DateTime.now - 10.hours,
+  end_datetime: DateTime.now + 1.days,
+  user_id: rand(0..users_number - 1)
+)
+Event.create(
+  title: 'My_event_2',
+  description: 'la - la - la',
+  begin_datetime: DateTime.now + 10.hours,
+  end_datetime: DateTime.now + 2.days,
+  user_id: rand(0..users_number - 1)
+)
+Event.create(
+  title: 'My_event_3',
+  description: 'la - la - la',
+  begin_datetime: DateTime.now - 2.days,
+  end_datetime: DateTime.now - 1.days,
+  user_id: rand(0..users_number - 1)
+)
 # Комнаты
 rooms_number.times do |i|
   Room.create(
-  title: "room_" + (i + rand(1..6)*10).to_s,
-  begin_work_time: Time.new(2000, 01, 01, 9, 0, 0, '+03:00'),
-  end_work_time: Time.new(2000, 01, 01, 18, 0, 0, '+03:00'),
-  description: "this is a room"
+    title: 'room_' + (i + rand(1..6) * 10).to_s,
+    begin_work_time: Time.new(2000, 0o1, 0o1, 9, 0, 0, '+03:00'),
+    end_work_time: Time.new(2000, 0o1, 0o1, 18, 0, 0, '+03:00'),
+    description: 'this is a room'
   )
 end
 
 Event.all.each do |e|
   Room.all.each do |r|
-    if rand(0..4) == 0
-      e.rooms << r
-    end
+    e.rooms << r if rand(0..4) == 0
   end
 end
 
@@ -57,10 +75,10 @@ end
 Room.all.each do |r|
   r.events.each_with_index do |e, i|
     Order.create(
-    begin_datetime: Time.new(2000, 01, 01, 9, 0, 0, '+03:00') + i.hours,
-    end_datetime: Time.new(2000, 01, 01, 9, 0, 0, '+03:00') + (i+1).hours,
-    room_id: r[:id],
-    event_id: e[:id]
+      begin_datetime: Time.new(2000, 0o1, 0o1, 9, 0, 0, '+03:00') + i.hours,
+      end_datetime: Time.new(2000, 0o1, 0o1, 9, 0, 0, '+03:00') + (i + 1).hours,
+      room_id: r[:id],
+      event_id: e[:id]
     )
   end
 end
