f072a461103b24db36aa187e7e4ad459d841d6b8
DaleOCBS <dale@ocbs.pro>
2017-06-29 12:48:13 +0300

    output of the nearest events fixed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @events_top = Event.where("end_datetime < ?", DateTime.now).limit(5)
+    @events_top = Event.where('end_datetime > ?', DateTime.now).order(:begin_datetime).limit(10)
   end
 end
