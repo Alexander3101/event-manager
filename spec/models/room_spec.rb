82bdfff188d5136c171f495213bc82f9499682f2
Darya Tikhomirova <tihodrik@gmail.com>
2017-06-29 15:03:32 +0300

    room tests fixed
    DateTime used instead of Time

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-
+  let(:begin_t) { DateTime.new(2017, 10, 10, 9, 0, 0, "+03:00") }
+  let(:end_t) { DateTime.new(2017, 10, 10, 19, 0, 0, "+03:00") }
   let(:room) do
     Room.new(
       title: "Test_room",
-      begin_work_time: Time.now,
-      end_work_time: Time.now + 12.hours,
+      begin_work_time: begin_t,
+      end_work_time: end_t,
       description: "elementary test room")
   end
 
   describe ".new" do
 
     it "is valid with valid attributes" do
-       expect(room).to be_valid
+      expect(room).to be_valid
     end
 
     context "if nil title is set" do
@@ -39,21 +40,21 @@ RSpec.describe Room, type: :model do
 
     context "id if end_work_time is earlier than begin_work_time (time)" do
       it "is not valid" do
-        room[:end_work_time] = event[:begin_work_time] - 15.minutes
+        room[:end_work_time] = room[:begin_work_time].change({minute: begin_t.min - 15})
         expect(room).not_to be_valid
       end
     end
 
     context "id if end_work_time is earlier than begin_work_time (date)" do
       it "is not valid" do
-        room[:end_work_time] = event[:end_work_time] - 1.day
+        room[:end_work_time] = room[:end_work_time].change({day: end_t.day - 1})
         expect(room).not_to be_valid
       end
     end
 
     context "if work time is longer than 1 day" do
       it "is not valid" do
-        room[:end_work_time] = event[:begin_work_time] + 2.days
+        room[:end_work_time] = room[:begin_work_time].change({day: end_t.day + 2})
         expect(room).not_to be_valid
       end
     end
