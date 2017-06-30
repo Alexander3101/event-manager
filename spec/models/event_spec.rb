74a617be65fdabf460ef62b44ce11cd2ede69112
Darya Tikhomirova <tihodrik@gmail.com>
2017-06-30 12:39:16 +0300

    event_spec beautified

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -20,61 +20,81 @@ RSpec.describe Event, type: :model do
        expect(event).to be_valid
     end
 
-    context "if nil title is set" do
-      it "is not valid" do
-        event[:title] = nil
-        expect(event).not_to be_valid
-      end
+    # TITLE tsts
+    context "TITLE tests" do
+    it "isn't valid if nil title is set" do
+      event[:title] = nil
+      expect(event).not_to be_valid
     end
+  end
 
-    context "if nil begin_datetime is set" do
-      it "is not valid" do
+    # BEGIN_DATEIME tests
+    context "BEGIN_DATEIME tests" do
+      it "isn't valid if nil begin_datetime is set" do
         event[:begin_datetime] = nil
         expect(event).not_to be_valid
       end
+
+      it "isn't valid if empty begin_datetime is set " do
+        event[:begin_datetime] = ""
+        expect(event).not_to be_valid
+      end
+
+      it "isn't valid if wrong begin_datetime is set " do
+        event[:begin_datetime] = "0o0o0"
+        expect(event).not_to be_valid
+      end
     end
 
-    context "if nil end_datetime is set" do
-      it "is not valid" do
+    # END_DATEIME tests
+    context "END_DATEIME tests" do
+      it "isn't valid if nil end_datetime is set" do
         event[:end_datetime] = nil
         expect(event).not_to be_valid
       end
-    end
 
-    context "if end_datetime is earlier than begin_datetime" do
-      it "is not valid" do
-        event[:end_datetime] = event[:begin_datetime] - 15.minutes
+      it "isn't valid if empty end_datetime is set" do
+        event[:end_datetime] = ""
+        expect(event).not_to be_valid
+      end
+
+      it "isn't valid if wrong end_datetime is set " do
+        event[:end_datetime] = "0o0o0"
         expect(event).not_to be_valid
       end
     end
 
-    context "if event length is less than 15 minutes" do
-      it "is not valid" do
+    #DATETIME tests
+    context "DATETIME tests" do
+      it "isn't valid if end_datetime is earlier than begin_datetime" do
+        event[:end_datetime] = event[:begin_datetime] - 15.minutes
+        expect(event).not_to be_valid
+      end
+
+      it "isn't valid if event length is less than 15 minutes" do
         event[:end_datetime] = event[:begin_datetime] + 2.minutes
         expect(event).not_to be_valid
       end
     end
 
-    context "if nil user_id is set" do
-      it "is not valid" do
+    # USER tests
+    context "USER tests" do
+      it "is not validif nil user_id is set" do
         event[:user_id] = nil
         expect(event).not_to be_valid
       end
-    end
 
-    context "if missed user_id is set" do
-      it "is not valid" do
+      it "is not valid if missed user_id is set" do
         event[:user_id] = u_id + 1
         expect(event).not_to be_valid
       end
-    end
 
-    context "if nil description is set" do
-      it "is valid" do
+      it "is not valid if nil description is set" do
         event[:description] = nil
         expect(event).to be_valid
       end
     end
+
   end
 
   describe ".delete" do
