6afe6a4d657d1b018f865200a58835b08ce0897e
ChipOCBS <chip@ocbs.pro>
2017-07-02 13:19:52 +0300

    more actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,9 +2,11 @@ class OrdersController < ApplicationController
   def index
     @orders = Order.where(room_id: params[:room_id])
   end
+
   def show
     @order = Order.find(params[:id])
   end
+
   def new
     @order = Order.new
     @events = Event.all
@@ -21,6 +23,20 @@ class OrdersController < ApplicationController
       redirect_to session.delete(:return_to)
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+
+    if @order.update(order_params)
+      redirect_to @order
+    else
+      render 'edit'
     end
   end
 
