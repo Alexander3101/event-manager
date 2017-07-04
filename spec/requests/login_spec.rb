c314f9e6be45db4945835a833fc6d611149544d4
Darya Tikhomirova <tihodrik@gmail.com>
2017-07-04 19:37:56 +0300

    Login/logout tests added

--- spec/requests/login_spec.rb
+++ spec/requests/login_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 Capybara.default_driver = :selenium
 
-RSpec.describe "LOGIN MODAL WINDOW tests", :type => :feature do
+RSpec.describe "LOGIN/LOGOUT MODAL WINDOW tests", :type => :feature do
   describe "content" do
 
     it "shows login modal window after pressing login button", js: true do
@@ -19,20 +19,61 @@ RSpec.describe "LOGIN MODAL WINDOW tests", :type => :feature do
     end
 
     def login!(login, password)
-      visit root_path
       click_on("Войти")
+
       within(".modal-content") do
         fill_in 'user_email', with: login
         fill_in 'user_password', with: password
         page.find(:xpath, '//input[@type="submit"]').click
+
       end
     end
 
-    it "logins", js: true do
+    it "logins and logouts from root_path", js: true do
+      visit root_path
       login!("admin@admins.net", "admin2")
+
       expect(page).to have_current_path(root_path)
       expect(page).to have_link("Выйти")
       expect(page).to have_link("Личный кабинет")
+
+      click_on("Выйти")
+      expect(page).to have_current_path(root_path)
+    end
+
+    it "logins/logouts from events_path", js: true do
+      visit events_path
+      login!("admin@admins.net", "admin2")
+
+      expect(page).to have_current_path(events_path)
+      expect(page).to have_link("Выйти")
+      expect(page).to have_link("Личный кабинет")
+
+      click_on("Выйти")
+      expect(page).to have_current_path(root_path)
+    end
+
+    it "logins/logouts from rooms_path", js: true do
+      visit rooms_path
+      login!("admin@admins.net", "admin2")
+
+      expect(page).to have_current_path(rooms_path)
+      expect(page).to have_link("Выйти")
+      expect(page).to have_link("Личный кабинет")
+
+      click_on("Выйти")
+      expect(page).to have_current_path(root_path)
+    end
+
+    it "has profile and can logout from profile page", js: true do
+      visit root_path
+      login!("admin@admins.net", "admin2")
+
+      click_on("Личный кабинет")
+      expect(page).to have_current_path("/users/" + (User.find_by email: "admin@admins.net")[:id].to_s)
+
+      click_on("Выйти")
+      expect(page).to have_current_path(root_path)
     end
   end
 
