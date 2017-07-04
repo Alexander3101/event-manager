56beb5d8b04a11a38ea91788162169f290cc7bf5
Darya Tikhomirova <tihodrik@gmail.com>
2017-07-04 09:04:17 +0300

    Home page tests added

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,11 +1,37 @@
 require 'rails_helper'
 Capybara.default_driver = :selenium
 
-RSpec.describe "HOME", :type => :feature do
-  describe "GET /" do
-    it "works!", js: true do
-      visit '/'
-      page.should have_content('О сервисе')
+RSpec.describe "HOME PAGE tests", :type => :feature do
+  describe "navbar" do
+    it "should has a navigation bar with Rooms, Events and Home links", js: true do
+      visit root_path
+      page.should have_link("Главная", :href=>"/", :class=>"navbar-brand")
+      page.should have_link("События", :href=>"/events")
+      page.should have_link("Комнаты", :href=>"/rooms")
+    end
+
+    it "goes to /events when Events button pressed", js: true do
+      visit root_path
+      click_link("События")
+      expect(page).to have_current_path("/events")
+    end
+
+    it "goes to /rooms when Rooms button pressed", js: true do
+      visit root_path
+      click_link("Комнаты")
+      expect(page).to have_current_path("/rooms")
+    end
+
+    it "has login button", js: true do
+      visit root_path
+      page.should have_link('Войти')
     end
   end
+  describe "content" do
+    it "should have 10 nearest events or less", js: true do
+      visit root_path
+      expect(page.source.should have_css('.my', :between => 0..10))
+    end
+  end
+
 end
