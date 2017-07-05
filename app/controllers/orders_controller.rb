d5c45ecd917784446e053e4934e205e90f14e558
Unknown <dale@ocbs.pro>
2017-07-05 14:03:08 +0300

    error message added for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,13 +17,12 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
-
     if @order.save
       unless @order.event.rooms.exists?(@order.room.id)
         @order.event.rooms << @order.room
       end
-      redirect_to session.delete(:return_to)
     else
+      flash[:notice] = @order.errors['text'].last
       redirect_to action: 'new', room_id: @order.room_id, event_id: @order.event_id
     end
   end
