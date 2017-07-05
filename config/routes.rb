d78b78ea87ad45259d8c5a8d8a6ac48373e44950
Unknown <chip@ocbs.pro>
2017-07-05 14:01:04 +0300

    beautifying with rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   get '/home', to: 'home#index', as: :home
-  get 'users/personal', as: 'user_root'
   get 'users/:id' => 'users#show'
   resources :events
   resources :rooms
--- db/migrate/20170622084335_devise_create_users.rb
+++ db/migrate/20170622084335_devise_create_users.rb
@@ -2,9 +2,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-      t.string :role,               null: false, default: "user"
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
+      t.string :role,               null: false, default: 'user'
 
       ## Recoverable
       # t.string   :reset_password_token
@@ -31,11 +31,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email, unique: true
     # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
