c6a0407ac6b4c4d1fed8004f382f37e3dfe35d81
Мартыненко Иван Евгеньевич <iwan@tversu.ru>
2018-02-07 09:10:22 +0300

    Update rooms index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,17 @@
 //= require calendar-initializer
 //
 // For debug purposes
+
+
+$(document).on('turbolinks:load', function() {
+  var max_height = 0;
+  var blocks = $('.card-body');
+  blocks.each(function() {
+    if ( $(this).height() > max_height ) {
+      max_height = $(this).height();
+    }
+  });
+  blocks.each(function() {
+    $(this).height(max_height);
+  });
+});
