d6385edee131acb2f94e4030afaef3f262914149
Мартыненко Иван Евгеньевич <iwan@tversu.ru>
2018-02-09 15:45:28 +0300

    Change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,14 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    dtn = DateTime.now
-    @events = Event.where(archive: false, user_id: @user.id).where("date > ? or (date = ? and end_time > ?)", dtn.strftime("%Y-%m-%d"), dtn.strftime("%Y-%m-%d"), dtn.strftime("%H:%M")).order(:date).order(:begin_time).paginate(page: params[:page], :per_page => 10)
-  end
-
-  def archive
-    @user = User.find(params[:id])
-    @events = Event.where(archive: true, user_id: @user.id).order(:date).reverse_order.order(:begin_time).paginate(page: params[:page], :per_page => 10)
+  def events
+    @events = Event.current_events(current_user.id)
   end
 
   def past
-    @user = User.find(params[:id])
-    dtn = DateTime.now
-    @events = Event.where(archive: false, user_id: @user.id).where("date <= ? or (date = ? and end_time <= ?)", dtn.strftime("%Y-%m-%d"), dtn.strftime("%Y-%m-%d"), dtn.strftime("%H:%M")).order(:date).reverse_order.order(:begin_time).paginate(page: params[:page], :per_page => 10)
+    @events = Event.past_events(current_user.id)
   end
 
+  def canceled
+    @events = Event.canceled_events(current_user.id)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get '/home', to: 'home#index', as: :home
 
   devise_for :users
-  get 'users/:id' => 'users#show'
-  get 'users/archive/:id', to: 'users#archive', as: :user_archive
-  get 'users/past/:id', to: 'users#past', as: :user_past
-  resources :users
+  get 'users/' => 'users#events'
+  get 'users/canceled/', to: 'users#canceled', as: :user_canceled
+  get 'users/past/', to: 'users#past', as: :user_past
+  # resources :users
 
   resources :events, except: [:show]
   get 'events/archive', to: 'events#archive', as: :archive
